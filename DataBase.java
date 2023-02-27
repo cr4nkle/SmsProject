94b0b57a6016d96f727f4c8a192d64089eca3f8c
Сергей Попович <cr4nkle@yandex.ru>
2023-02-27 10:16:53 +0800

    Создал класс, который реализует абстрактный

---
+++ DataBase.java
@@ -0,0 +1,14 @@
+public class DataBase extends MySQLHelper{
+    private static DataBase INSTANCE;
+
+    private DataBase(){
+        initDB();
+    }
+
+    public static synchronized DataBase getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new DataBase();
+        }
+        return INSTANCE;
+    }
+}
